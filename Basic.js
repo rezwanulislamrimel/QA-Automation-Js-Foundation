29a6cda20c957de380cb824689e8c649b82cc727
Rimel <rezwanulrimels@gmail.com>
2025-12-26 15:50:58 +0600

    variable and constant added

--- Basic.js
+++ Basic.js
@@ -2,3 +2,12 @@
 let name='automation';
 
 console.log('Course Name:', name)
+
+let Age=1.5;
+console.log('Course Age:', Age)
+const isPublished=true;
+console.log('Is Published:', isPublished)
+
+const rating=4.5;
+console.log('Course Rating:', rating)
+
